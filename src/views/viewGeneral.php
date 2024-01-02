89f707ea84415f0c6714b640f7c6c37880ab0f1a
Jean Hubert <izoret.raphael@gmail.com>
2024-01-02 16:13:32 +0100

    css pog

--- src/views/viewGeneral.php
+++ src/views/viewGeneral.php
@@ -23,10 +23,12 @@ use NP\Lib\Translation as T;
 <body>
 <header>
     <div id="iconsAndPageTitle">
-        <a href="?action=SwitchTheme">
-            <img id="theme_icon" alt="theme switching icon"
-                 src="<?= W::getSiteRoot() ?>/ressources/images/icons/theme_<?= Theme::getCurrentTheme() ?>.png">
-        </a>
+        <div id="theme_icon_div">
+            <a href="?action=switchTheme">
+                <img id="theme_icon" alt="theme switching icon"
+                     src="<?= W::getSiteRoot() ?>/ressources/images/icons/theme_<?= Theme::getCurrentTheme() ?>.png">
+            </a>
+        </div>
 
         <h1>Raphaël Izoret - <?= $pageTitle ?></h1>
 
@@ -35,17 +37,23 @@ use NP\Lib\Translation as T;
                  src="<?= W::getSiteRoot() ?>/ressources/images/icons/language_<?= T::getCurrentLanguage() ?>.png"
                  onclick="spawnLanguageDropdown()">
             <div id="language_dropdown" class="hide">
-                <a href="?action=switchLanguage&lang=fr"><?= T::translate('Français', 'French')?></a>
-                <a href="?action=switchLanguage&lang=en"><?= T::translate('Anglais', 'English')?></a>
+                <a href="?action=switchLanguage&lang=fr"><?= T::translate('Français', 'French') ?></a>
+                <a href="?action=switchLanguage&lang=en"><?= T::translate('Anglais', 'English') ?></a>
             </div>
         </div>
     </div>
     <nav>
         <div id="sectionsMenu">
-            <div class="sectionsMenuItem"><a href="?controller=Presentation&action=displayIndex"><?=T::getTitleElem_Presentation()?></a></div>
-            <div class="sectionsMenuItem"><a href="?controller=CV&action=displayIndex"><?=T::getTitleElem_CV()?></a></div>
-            <div class="sectionsMenuItem"><a href="?controller=Projection&action=displayIndex"><?=T::getTitleElem_Projet()?></a></div>
-            <div class="sectionsMenuItem"><a href="?controller=Realisations&action=displayIndex"><?=T::getTitleElem_Realisations()?></a></div>
+            <div class="sectionsMenuItem"><a
+                        href="?controller=Presentation&action=displayIndex"><?= T::getTitleElem_Presentation() ?></a>
+            </div>
+            <div class="sectionsMenuItem"><a href="?controller=CV&action=displayIndex"><?= T::getTitleElem_CV() ?></a>
+            </div>
+            <div class="sectionsMenuItem"><a
+                        href="?controller=Projection&action=displayIndex"><?= T::getTitleElem_Projet() ?></a></div>
+            <div class="sectionsMenuItem"><a
+                        href="?controller=Realisations&action=displayIndex"><?= T::getTitleElem_Realisations() ?></a>
+            </div>
         </div>
     </nav>
 </header>
@@ -57,13 +65,13 @@ use NP\Lib\Translation as T;
 <footer>
     <p>--------------------
         <?= T::translate(
-        '<br>Portfolio de Raphaël Izoret
+            '<br>Portfolio de Raphaël Izoret
         <br>Tous droits réservés
-        <br><a href="' . \NP\Configuration\WebsiteConfiguration::getSourceCode() . '">Code source</a>'
-        , '<br>Portfolio of Raphaël Izoret
+        <br><a href="' . W::getSourceCode() . '">Code source</a>'
+            , '<br>Portfolio of Raphaël Izoret
         <br>All rights reserved
-        <br><a href="' . \NP\Configuration\WebsiteConfiguration::getSourceCode() . '">Source code</a>'
-        )?>
+        <br><a href="' . W::getSourceCode() . '">Source code</a>'
+        ) ?>
         <br>--------------------
     </p>
 </footer>
